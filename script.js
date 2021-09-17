6f7f66f8e73cc0225c2092f17b14b3c6eb725e5e
Ayaka Yasuda <ayakayasuda23@gmail.com>
2021-09-17 12:16:05 -0700

    finish

---
+++ script.js
@@ -0,0 +1,61 @@
+'use strict';
+
+let chickens = 1;
+chickens = 2;
+console.log(chickens);
+
+// const apples = 1;
+// apples = 2;
+
+// let age = 18;
+// var age = 20;
+
+var age = 18;
+var age = 20;
+console.log(age);
+
+console.log(10 / 2 + (6 + 4) * 2 - 5);
+console.log(1 + 2 * (4 % 3));
+console.log('4' + '3');
+console.log(2 + 5 + '8');
+
+let x = 2;
+let y = 2;
+let z = 2;
+console.log((z = x + y));
+
+console.log(3 * 4 + (5 + 6) * 1 * 0);
+console.log(1 + '2' + 4 + 2);
+console.log(10 - '3' * 2 + 3);
+console.log(102 % 5);
+console.log(1 + 5 + 'bottles of milk');
+
+let a = 3
+let b = 2
+let c = a
+a = b = c = 1
+console.log(a);
+
+const myVal = '5'
+let myAddedVal = myVal + 10;
+console.log(myAddedVal);
+
+let n = 3
+let m = n++
+console.log(m);
+
+console.log("1 plus 1 is " + 1 + 1);
+
+console.log(typeof 4);
+console.log(typeof "4");
+console.log(typeof 4.0);
+console.log(typeof -4);
+
+// let age1 = prompt("Enter your age");
+// let next = age1 + 1;
+// alert("On your next birthday, you'll be "+next);
+
+console.log(10 ** 2);
+console.log(Math.pow(10,2));
+
+console.log(typeof NaN);
